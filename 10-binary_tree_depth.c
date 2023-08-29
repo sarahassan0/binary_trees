7fdb23f87ba2e1fc8596ae522a02dea3c9ce2d85
sarahassan0 <sarraree5@gmail.com>
2023-08-29 20:17:04 +0300

    fix betty

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_depth - measures the height of a binary tree
+ * @tree: pointer to the root node of the tree to measure the depth.
+ * Return: If tree is NULL, must return 0
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	binary_tree_t *root = tree->parent;
+	size_t depth = 0;
+
+	while (root)
+	{
+		root = root->parent;
+		depth++;
+	}
+	return (depth);
+}
