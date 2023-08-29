9310d1e4ccd51dec6d15ddeacfb719c3716b6d1d
sarahassan0 <sarraree5@gmail.com>
2023-08-29 23:35:34 +0300

    ...

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -5,19 +5,29 @@
  * Return: 0 if tree is NULL, otherwise 1.
  */
 
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height, right_height;
+
+	left_height = 0, right_height = 0;
+	if (!tree)
+		return (0);
+	if (tree->left)
+		left_height = binary_tree_height(tree->left) + 1;
+	if (tree->right)
+		right_height = binary_tree_height(tree->right) + 1;
+
+	return ((left_height > right_height) ? left_height : right_height);
+}
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int left_leaf = 0, right_leaf = 0;
+	int left_leaf, right_leaf;
 
-	if (!tree ||(!tree->left && !tree->right))
-			return (0);
-	if ((tree->left && tree->right) || (!tree->left && !tree->right))
-	{
-		left_leaf++;
-	    right_leaf++;
-	}
-	left_leaf= binary_tree_is_perfect(tree->left);
-	right_leaf = binary_tree_is_perfect(tree->right);
+	if (!tree || (!tree->left && !tree->right))
+		return (0);
+	left_leaf = binary_tree_height(tree->left);
+	right_leaf = binary_tree_height(tree->right);
+
 
 	return (left_leaf == right_leaf);
 }
