678608037a298c5fb110bd0b41955993fefe42f5
Sarah Hassan <sarraree5@gmail.com>
2023-08-30 02:14:11 +0300

    Update 17-binary_tree_sibling.c

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -9,7 +9,7 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 	binary_tree_t *parent = node->parent;
 
-	if (!node || !parent || (!parent->left && !parent->right))
+	if (!node || !parent)
 		return (NULL);
 
 	return ((node == parent->left) ? parent->right : parent->left);
