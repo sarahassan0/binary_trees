6cb6379c2f8a2c8e4afdbd5ab97b9512b41c7a0b
Sarah Hassan <sarraree5@gmail.com>
2023-08-30 01:07:03 +0300

    Update

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -11,9 +11,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 
 	if (!tree || (tree->parent && (!tree->left && !tree->right)))
 		return (0);
-	// if((!tree->left && !tree->right) || (tree->left && tree->right))
-	//     return (1);
-	// else return (0);
+
 	left_leaf = binary_tree_is_perfect(tree->left);
 	right_leaf = binary_tree_is_perfect(tree->right);
 	return (left_leaf == right_leaf);
