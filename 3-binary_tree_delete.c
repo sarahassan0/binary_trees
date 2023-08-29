732739755d10709861c6818db1a43a7329951508
abeermosaad <abeermosaad69@gmail.com>
2023-08-29 15:39:21 +0300

    Betty is happy

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,9 +1,8 @@
 #include "binary_trees.h"
 /**
- * binary_tree_insert_left - inserts a node as the left-child of another node
- * @parent: pointer to the node to insert the left-child in
- * @value: value to store in the new node
- * Return: created node, or NULL on failure or if parent is NULL
+ * binary_tree_delete - deletes an entire binary tree
+ * @tree: pointer to the root node of the tree to delete
+ * Return: Nothing
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
