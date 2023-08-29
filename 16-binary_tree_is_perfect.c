b4def6acaea5efca289c4eefaf404a0e68754c24
Sarah Hassan <sarraree5@gmail.com>
2023-08-30 01:35:10 +0300

    Update 16-binary_tree_is_perfect.c

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -25,7 +25,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
     int left_height, right_height;
 
     if (!tree)
-        return (1);
+        return (0);
 
     if (!tree->left && !tree->right)
         return (1);
