4e561f664e9490503774486881cd4df7905dc9e5
sarahassan0 <sarraree5@gmail.com>
2023-08-30 19:24:48 +0300

    ..

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -7,16 +7,15 @@
  */
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (!tree || !func)
+	if (!tree || !func || !tree->left || !tree->right)
 		return;
 	if (!tree->parent)
 	{
 		func(tree->n);
 	}
-    if(tree->left)
-        func(tree->left->n);
-    if(tree->right)
-	    func(tree->right->n);
+
+	func(tree->left->n);
+	func(tree->right->n);
 	binary_tree_levelorder(tree->left, func);
 
 	binary_tree_levelorder(tree->right, func);
