a18ab1e02f77dc01d8226459661b9fd58adccba2
sarahassan0 <sarraree5@gmail.com>
2023-08-30 17:32:07 +0300

    task 19

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -12,18 +12,20 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tr
 	if (!first || !second)
 		return (NULL);
 
-	while (first)
-	{
-		while (second)
-		{
-			if ((first->parent == second->parent) ||
+    while(first)
+    {
+        while (second)
+        {
+            if (first->parent == second->parent ||
 					(first->parent == second))
-				return (first->parent);
-			if (first == second->parent)
-				return (second->parent);
-		}
-		first = first->parent;
-	}
+                return (first->parent);
+            if(first == second->parent)
+                return second->parent;
+            second = second->parent;
+        }
+    first = first->parent;
+    }
+
 
 	return (NULL);
 }
