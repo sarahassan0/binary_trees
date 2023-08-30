b9d6a4b9a89be9358ff125ae61af8f67fb940dda
sarahassan0 <sarraree5@gmail.com>
2023-08-30 03:22:05 +0300

    fix

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -7,9 +7,6 @@
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	binary_tree_t *parent = node->parent;
-	binary_tree_t *grand_p = parent->parent;
-
 	if (!node || !node->parent || !node->parent->parent
 				|| !node->parent->parent->left || !node->parent->parent->right)
 		return (NULL);
