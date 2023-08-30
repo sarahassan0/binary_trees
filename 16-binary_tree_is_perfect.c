972f1e44baac45c7f90dadfbeb70a53cab096356
sarahassan0 <sarraree5@gmail.com>
2023-08-30 05:23:24 +0300

    fix the file tree

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -42,3 +42,18 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	return (binary_tree_is_perfect(tree->left) &&
 	binary_tree_is_perfect(tree->right));
 }
+
+/*
+ * this code has one red check
+ *  int binary_tree_is_perfect(const binary_tree_t *tree)
+    {
+    int left_leaf, right_leaf;
+
+	if (!tree || (tree->parent && (!tree->left && !tree->right)))
+		return (0);
+
+	left_leaf = binary_tree_is_perfect(tree->left);
+	right_leaf = binary_tree_is_perfect(tree->right);
+	return (left_leaf == right_leaf);
+}
+*/
