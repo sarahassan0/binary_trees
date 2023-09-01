79c5775ca552c90835a58e43a97edb53e2fb603e
sarahassan0 <sarraree5@gmail.com>
2023-09-01 03:00:23 +0300

    .

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -4,15 +4,13 @@
  * @tree: pointer to the root node of the tree to check
  * Return: 1 if tree is a valid BST, and 0 otherwise
  */
-
 int binary_tree_is_bst(const binary_tree_t *tree)
 {
     int left = 1, right = 1;
     int l, r;
 
     if (!tree)
-        return (1);
-
+        return 1;
     if (tree->left && (tree->left->n > tree->n))
         left = 0;
     if (tree->right && (tree->n >= tree->right->n))
